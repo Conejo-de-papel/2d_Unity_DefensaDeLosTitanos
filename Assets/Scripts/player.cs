aa21b4255951e5d0e477d1fe8632492c18d95037
Lana <135757789+Conejo-de-papel@users.noreply.github.com>
2024-06-24 02:09:11 -0300

    Initial commit

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    public float moveSpeed; // Скорость движения
+    Rigidbody2D rb; // Компонент Rigidbody2D
+
+    private Vector2 targetPosition; // Целевая позиция
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = rb.position; // Инициализация целевой позиции текущим положением
+    }
+
+    void Update()
+    {
+        // Обновляем целевую позицию при клике мыши
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Перемещение к целевой позиции
+        if ((Vector2)rb.position != targetPosition)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rb.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(newPosition);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
